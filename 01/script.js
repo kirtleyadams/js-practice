cb52df52345dabe94688ae9fcddd046a17c5e3a9
Kirtley Adams <92392080+kirtleyadams@users.noreply.github.com>
2022-07-12 20:08:26 -0400

    first

---
+++ 01/script.js
@@ -0,0 +1,25 @@
+Write a function that takes in an array of numbers and outputs the maximum number.
+
+Ex:
+Input: [ 1, 2, 3 ]
+Output: 3
+
+Input: [ 3, 6, 4, 5, 2, 1 ]
+Output: 6
+
+Input: [ 3, 3, 3 ]
+Output: 3
+
+function findMax(inAry) {
+    let currentMax = inAry[0];
+    //find the max num in an array
+    //loop through all the items in the array (for loop)
+    for (let index = 0; index < inAry.length; index++) {
+        const currentNumber = inAry[index];
+        
+    }
+    //get current numebr from array using for index value
+    //compare the current number (in the loop) to the current max (put in a variable..)
+    return currentMax;
+}
+const theMax = findMax ([1, 2, 3];)
