8021c2286377cd7abb413bef407e8c2dcf4cb628
Kirtley Adams <92392080+kirtleyadams@users.noreply.github.com>
2022-07-14 20:00:03 -0400

    02

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,4 +1,4 @@
-Write a function that takes in an array of numbers and outputs the maximum number.
+//Write a function that takes in an array of numbers and outputs the maximum number.
 
 Ex:
 Input: [ 1, 2, 3 ]
@@ -11,15 +11,18 @@ Input: [ 3, 3, 3 ]
 Output: 3
 
 function findMax(inAry) {
-    let currentMax = inAry[0];
     //find the max num in an array
+    let currentMax = inAry[0];
     //loop through all the items in the array (for loop)
     for (let index = 0; index < inAry.length; index++) {
         const currentNumber = inAry[index];
-        
+        if (currentNumber > currentMax) {
+            currentMax = currentNumber;
+        }
     }
     //get current numebr from array using for index value
     //compare the current number (in the loop) to the current max (put in a variable..)
     return currentMax;
 }
-const theMax = findMax ([1, 2, 3];)
+const theMax = findMax ([1, 2, 3]);
+console.log (`Max: ${theMax}`);
---
+++ 02/script.js
@@ -0,0 +1,22 @@
+//Write a function that takes in a string and outputs the number of vowels (not counting y).
+
+// Ex:
+// Input: "hello"
+// Output: 2
+
+// Input: "you are great!"
+// Output: 6
+
+// Input: "why?"
+// Output: 0
+
+//variables
+var input = "hello"
+//function(s)
+    //parameter is a string
+    //first input is hello...
+    //set vowelCount = 0
+    //comparison operator (if)... if ("hello")
+
+    
+//logic//call function
