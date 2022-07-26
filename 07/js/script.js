1ff3f5c5da63258110ad8320ecbf6f7aa79ebbcb
Kirtley Adams <92392080+kirtleyadams@users.noreply.github.com>
2022-07-26 19:46:19 -0400

    07

---
+++ 07/js/script.js
@@ -0,0 +1,11 @@
+Write a function that takes in an input and returns true if it's an integer and false otherwise.
+
+Ex:
+Input: "7"
+Output: false
+
+Input: 7
+Output: true
+
+Input: 4.3
+Output: false
