ccc867666560d5c8d0ff98ec492c848d2af34f13
Kirtley Adams <92392080+kirtleyadams@users.noreply.github.com>
2022-07-28 20:08:38 -0400

    98

---
+++ 08/js/script.js
@@ -0,0 +1,17 @@
+// //Write a function that takes an array of integers as an input and outputs the first duplicate
+// ex:
+// input: [2,5,6,2,5]
+// output: 5
+
+// input: [1,3,4,1,3,4]
+// output: 1
+
+// input: [2,4,5]
+// output: undefined
+
+const input1: [2,5,6,2,5];
+function findDup(inAry) {
+    return 42;
+}
+
+findDup()
