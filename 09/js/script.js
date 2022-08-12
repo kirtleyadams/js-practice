b7f965e73ddbd3b602a29cc83a43e3a9e382d7b2
Kirtley Adams <92392080+kirtleyadams@users.noreply.github.com>
2022-08-11 20:04:11 -0400

    09

---
+++ 09/js/script.js
@@ -0,0 +1,11 @@
+Write a function that takes in a number and returns the corresponding day of the week.
+
+Ex:
+Input: 1
+Output: 'Monday'
+
+Input: 5
+Output: 'Friday'
+
+Input: 8
+Output: undefined
