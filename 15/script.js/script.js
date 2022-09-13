d01774162ae1040ffd9a96a3efc7c930e57342d2
Kirtley Adams <92392080+kirtleyadams@users.noreply.github.com>
2022-09-12 21:05:06 -0400

    solved

--- 15/script.js/script.js
+++ 15/script.js/script.js
@@ -1,11 +1,26 @@
 // Write a function that takes in a number and reverses the order of the digits.
 
 // Ex:
-Input: 1234
-Output:4321
+// Input: 1234
+// Output:4321
 
-Input: 1201
-Output:1021
+// Input: 1201
+// Output:1021
 
-Input: 4
-Output: 4
+// Input: 4
+// Output: 4
+
+const input = [1234, 1201, 4];
+
+const reverseIt = (num) =>
+  parseInt(num.toString().split("").reverse().join(""));
+
+input.forEach((element) => {
+  console.log(`${element} reversed is ${reverseIt(element)}`);
+});
+
+
+
+
+
+
